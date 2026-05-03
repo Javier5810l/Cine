d6d44f423a7380ab3436701c403750fcae646262
unknown <alonso_c.c@hotmail.com>
2026-05-02 23:36:37 -0500

    first commit

---
+++ Script.js
@@ -0,0 +1,23 @@
+let contadorNo = 0;
+
+function responderSi() {
+    document.getElementById("mensaje").innerText = "¡Genial! Has aceptado 😄 Fin del proceso.";
+    document.getElementById("contador").innerText = "";
+}
+
+function responderNo() {
+    contadorNo++;
+
+    let mensajes = [
+        "¿Segura? 🤔",
+        "Piénsalo otra vez 😅",
+        "Vamos... intenta de nuevo 😆",
+        "Ya van varios intentos 👀",
+        "Ok... pero sabes que terminarás diciendo que sí 😏"
+    ];
+
+    let mensaje = mensajes[(contadorNo - 1) % mensajes.length];
+
+    document.getElementById("mensaje").innerText = mensaje;
+    document.getElementById("contador").innerText = "Veces que presionaste 'No': " + contadorNo;
+}
